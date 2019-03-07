05ae239e943b30fd4710ba5a02857c4f1b267184
Arnaud Garin <arnaud.garin@epfl.ch>
2019-03-07 17:44:55 +0100

    Columns store done

--- src/main/java/ch/epfl/dias/store/column/ColumnStore.java
+++ src/main/java/ch/epfl/dias/store/column/ColumnStore.java
@@ -6,29 +6,71 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.epfl.dias.store.DataType;
 import ch.epfl.dias.store.Store;
 
 public class ColumnStore extends Store {
 
 	// TODO: Add required structures
+	private DataType[] schema;
+	private String filename;
+	private String delimiter;
+	private boolean lateMaterialization;
+	private DBColumn[] col_data;
 
 	public ColumnStore(DataType[] schema, String filename, String delimiter) {
 		this(schema, filename, delimiter, false);
 	}
 
 	public ColumnStore(DataType[] schema, String filename, String delimiter, boolean lateMaterialization) {
-		// TODO: Implement
+		this.schema = schema;
+		this.filename = filename;
+		this.delimiter = delimiter;
+		this.lateMaterialization = lateMaterialization;
+		col_data = new DBColumn[schema.length]; // Number of elem in schema = number of columns
 	}
 
 	@Override
 	public void load() throws IOException {
-		// TODO: Implement
+		BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+
+		for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+			String[] tuple = line.split(this.delimiter);
+
+			for (int i = 0; i < tuple.length; i++){
+				// TODO Implement columns first
+			}
+		}
 	}
 
 	@Override
 	public DBColumn[] getColumns(int[] columnsToGet) {
 		// TODO: Implement
 		return null;
+	}
+
+	private Object elemParser(String elem, DataType type) throws IOException{
+		Object ret = null;
+		switch (type){
+			case INT:
+				ret = Integer.parseInt(elem);
+				break;
+			case DOUBLE:
+				ret = Double.parseDouble(elem);
+				break;
+			case STRING:
+				ret = elem; // Same type
+				break;
+			case BOOLEAN:
+				ret = Boolean.parseBoolean(elem);
+				break;
+			default:
+				throw new IOException("Error");
+		}
+
+		return ret;
 	}
 }
--- src/main/java/ch/epfl/dias/store/column/DBColumn.java
+++ src/main/java/ch/epfl/dias/store/column/DBColumn.java
@@ -6,6 +6,12 @@ import java.util.Arrays;
 import ch.epfl.dias.store.DataType;
 
 public class DBColumn {
+	/*
+	This will besaically be a list of object of any type
+	Maybe it will also hold the type of the column who knows
+	Probably will use the same logic as row stor and have four
+	different getAs'bla' methods
+	 */
 
 	// TODO: Implement
 	
