e565cfe0bd30404b6cf7740e456d5b22660ba62e
Arnaud Garin <arnaud.garin@epfl.ch>
2019-03-18 19:00:37 +0100

    First operator implementation

--- src/main/java/ch/epfl/dias/ops/columnar/Project.java
+++ src/main/java/ch/epfl/dias/ops/columnar/Project.java
@@ -1,17 +1,25 @@
 package ch.epfl.dias.ops.columnar;
 
+import ch.epfl.dias.store.column.ColumnStore;
 import ch.epfl.dias.store.column.DBColumn;
 
 public class Project implements ColumnarOperator {
 
-	// TODO: Add required structures
+	private ColumnarOperator child;
+	private int[] columns;
 
 	public Project(ColumnarOperator child, int[] columns) {
-		// TODO: Implement
+		this.child = child;
+		this.columns = columns;
 	}
 
 	public DBColumn[] execute() {
-		// TODO: Implement
-		return null;
+		DBColumn[] elem = this.child.execute();
+		DBColumn[] ret = new DBColumn[this.columns.length];
+
+		for(int i=0; i < this.columns.length; i++){
+			ret[i] = elem[this.columns[i]];
+		}
+		return ret;
 	}
 }
--- src/main/java/ch/epfl/dias/ops/columnar/Scan.java
+++ src/main/java/ch/epfl/dias/ops/columnar/Scan.java
@@ -3,17 +3,24 @@ package ch.epfl.dias.ops.columnar;
 import ch.epfl.dias.store.column.ColumnStore;
 import ch.epfl.dias.store.column.DBColumn;
 
+import java.util.stream.IntStream;
+
 public class Scan implements ColumnarOperator {
 
-	// TODO: Add required structures
+	private ColumnStore store;
 
 	public Scan(ColumnStore store) {
-		// TODO: Implement
+		this.store = store;
 	}
 
 	@Override
 	public DBColumn[] execute() {
-		// TODO: Implement
-		return null;
+		if (this.store.isLateMaterialization()){
+			return null;
+		}else{
+			//int[] a = IntStream.range(0, this.store.getCol_data().length).toArray();
+			return this.store.getCol_data();
+		}
+
 	}
 }
