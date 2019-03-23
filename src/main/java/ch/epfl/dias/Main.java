d0d1273bb631adc3778ab36ac6edfca6925af799
Arnaud Garin <arnaud_lhc@hotmail.com>
2019-03-23 16:14:06 +0100

    Queries done

--- src/main/java/ch/epfl/dias/Main.java
+++ src/main/java/ch/epfl/dias/Main.java
@@ -154,6 +154,17 @@ public class Main {
 			paxstoreLineItem.load();
 			System.out.println("END");
 
+			System.out.println("Query 1 :");
+			System.out.println(queryOnePax(paxstoreLineItem));
+			System.out.println("Query 2 :");
+			System.out.println(queryTwoPax(paxstoreLineItem));
+			System.out.println("Query 3 :");
+			System.out.println(queryThreePax(paxstoreLineItem));
+			System.out.println("Query 4 :");
+			System.out.println(queryFourPax(paxstoreLineItem));
+			System.out.println("Query 5 :");
+			System.out.println(queryFivePax(paxstoreLineItem, paxstoreOrder));
+
 
 
 		}catch (Exception e){
@@ -175,6 +186,30 @@ public class Main {
 			columnstoreLineItem.load();
 			System.out.println("END");
 
+			System.out.println("Columnar query");
+			System.out.println("Query 1 :");
+			System.out.println(queryOneCol(columnstoreLineItem));
+			System.out.println("Query 2 :");
+			System.out.println(queryTwoCol(columnstoreLineItem));
+			System.out.println("Query 3 :");
+			System.out.println(queryThreeCol(columnstoreLineItem));
+			System.out.println("Query 4 :");
+			System.out.println(queryFourCol(columnstoreLineItem));
+			System.out.println("Query 5 :");
+			System.out.println(queryFiveCol(columnstoreLineItem, columnstoreOrder));
+
+			System.out.println("Vector query");
+			System.out.println("Query 1 :");
+			System.out.println(queryOneVec(columnstoreLineItem));
+			System.out.println("Query 2 :");
+			System.out.println(queryTwoVec(columnstoreLineItem));
+			System.out.println("Query 3 :");
+			System.out.println(queryThreeVec(columnstoreLineItem));
+			System.out.println("Query 4 :");
+			System.out.println(queryFourVec(columnstoreLineItem));
+			System.out.println("Query 5 :");
+			System.out.println(queryFiveVec(columnstoreLineItem, columnstoreOrder));
+
 		}catch (Exception e){
 			System.out.println(e);
 		}finally {
@@ -184,6 +219,8 @@ public class Main {
 		}
 		System.out.println("Finished");
 	}
+
+	// ROW QUERY
 	static public long queryOneRow(RowStore lineItem){
 		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 == 6
 		ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(lineItem);
@@ -240,22 +277,232 @@ public class Main {
 	}
 
 	static public long queryFiveRow(RowStore lineItem, RowStore orders){
-		/* SELECT COUNT(*) FROM order JOIN lineitem ON (o_orderkey = orderkey) WHERE orderkey = 3;*/
+		// SELECT COUNT(*) FROM order JOIN lineitem ON (o_orderkey = orderkey)
 
 		ch.epfl.dias.ops.volcano.Scan scanOrder = new ch.epfl.dias.ops.volcano.Scan(orders);
 		ch.epfl.dias.ops.volcano.Scan scanLineitem = new ch.epfl.dias.ops.volcano.Scan(lineItem);
 
-		/*Filtering on both sides */
-		Select selOrder = new Select(scanOrder, BinaryOp.EQ,0,3);
-		Select selLineitem = new Select(scanLineitem, BinaryOp.EQ,0,3);
-
-		HashJoin join = new HashJoin(selOrder,selLineitem,0,0);
+		HashJoin join = new HashJoin(scanOrder,scanLineitem,0,0);
 		ProjectAggregate agg = new ProjectAggregate(join,Aggregate.COUNT, DataType.INT,0);
 
 		long startTime = System.nanoTime();
 		agg.open();
 		DBTuple result = agg.next();
 		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	// PAX QUERY
+	static public long queryOnePax(PAXStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 == 6
+		ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(lineItem);
+		ch.epfl.dias.ops.volcano.Select sel = new ch.epfl.dias.ops.volcano.Select(scan, BinaryOp.EQ, 3, 6);
+		ch.epfl.dias.ops.volcano.Project prj = new ch.epfl.dias.ops.volcano.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		prj.open();
+		DBTuple result = prj.next();
+		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryTwoPax(PAXStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(lineItem);
+		ch.epfl.dias.ops.volcano.Select sel = new ch.epfl.dias.ops.volcano.Select(scan, BinaryOp.GT, 3, 6);
+		ch.epfl.dias.ops.volcano.Project prj = new ch.epfl.dias.ops.volcano.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		prj.open();
+		DBTuple result = prj.next();
+		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryThreePax(PAXStore lineItem){
+		// SELECT COUNT (*) FROM LINE_ITEM
+		ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(lineItem);
+		ch.epfl.dias.ops.volcano.ProjectAggregate agg = new ch.epfl.dias.ops.volcano.ProjectAggregate(scan, Aggregate.COUNT, DataType.INT, 3);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBTuple result = agg.next();
+		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFourPax(PAXStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(lineItem);
+		ch.epfl.dias.ops.volcano.Select sel = new ch.epfl.dias.ops.volcano.Select(scan, BinaryOp.GE, 3, 6);
+		ch.epfl.dias.ops.volcano.ProjectAggregate agg = new ch.epfl.dias.ops.volcano.ProjectAggregate(sel, Aggregate.AVG, DataType.DOUBLE, 4);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBTuple result = agg.next();
+		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFivePax(PAXStore lineItem, PAXStore orders){
+		// SELECT COUNT(*) FROM order JOIN lineitem ON (o_orderkey = orderkey)
+
+		ch.epfl.dias.ops.volcano.Scan scanOrder = new ch.epfl.dias.ops.volcano.Scan(orders);
+		ch.epfl.dias.ops.volcano.Scan scanLineitem = new ch.epfl.dias.ops.volcano.Scan(lineItem);
+
+		HashJoin join = new HashJoin(scanOrder,scanLineitem,0,0);
+		ProjectAggregate agg = new ProjectAggregate(join,Aggregate.COUNT, DataType.INT,0);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBTuple result = agg.next();
+		System.out.println(result);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	// COLUMN QUERY
+	static public long queryOneCol(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 == 6
+		ch.epfl.dias.ops.columnar.Scan scan = new ch.epfl.dias.ops.columnar.Scan(lineItem);
+		ch.epfl.dias.ops.columnar.Select sel = new ch.epfl.dias.ops.columnar.Select(scan, BinaryOp.EQ, 3, 6);
+		ch.epfl.dias.ops.columnar.Project prj = new ch.epfl.dias.ops.columnar.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		DBColumn[] result = prj.execute();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryTwoCol(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.columnar.Scan scan = new ch.epfl.dias.ops.columnar.Scan(lineItem);
+		ch.epfl.dias.ops.columnar.Select sel = new ch.epfl.dias.ops.columnar.Select(scan, BinaryOp.GT, 3, 6);
+		ch.epfl.dias.ops.columnar.Project prj = new ch.epfl.dias.ops.columnar.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		DBColumn[] result = prj.execute();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryThreeCol(ColumnStore lineItem){
+		// SELECT COUNT (*) FROM LINE_ITEM
+		ch.epfl.dias.ops.columnar.Scan scan = new ch.epfl.dias.ops.columnar.Scan(lineItem);
+		ch.epfl.dias.ops.columnar.ProjectAggregate agg = new ch.epfl.dias.ops.columnar.ProjectAggregate(scan, Aggregate.COUNT, DataType.INT, 3);
+
+		long startTime = System.nanoTime();
+		DBColumn[] result = agg.execute();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFourCol(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.columnar.Scan scan = new ch.epfl.dias.ops.columnar.Scan(lineItem);
+		ch.epfl.dias.ops.columnar.Select sel = new ch.epfl.dias.ops.columnar.Select(scan, BinaryOp.GE, 3, 6);
+		ch.epfl.dias.ops.columnar.ProjectAggregate agg = new ch.epfl.dias.ops.columnar.ProjectAggregate(sel, Aggregate.AVG, DataType.DOUBLE, 4);
+
+		long startTime = System.nanoTime();
+		DBColumn[] result = agg.execute();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFiveCol(ColumnStore lineItem, ColumnStore orders){
+		// SELECT COUNT(*) FROM order JOIN lineitem ON (o_orderkey = orderkey)
+
+		ch.epfl.dias.ops.columnar.Scan scanOrder = new ch.epfl.dias.ops.columnar.Scan(orders);
+		ch.epfl.dias.ops.columnar.Scan scanLineitem = new ch.epfl.dias.ops.columnar.Scan(lineItem);
+
+		ch.epfl.dias.ops.columnar.Join join = new ch.epfl.dias.ops.columnar.Join(scanLineitem, scanOrder, 0, 0);
+		ch.epfl.dias.ops.columnar.ProjectAggregate agg = new ch.epfl.dias.ops.columnar.ProjectAggregate(join, Aggregate.COUNT, DataType.INT, 0);
+
+		long startTime = System.nanoTime();
+		DBColumn[] result = agg.execute();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	// VECTOR QUERY
+	static public long queryOneVec(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 == 6
+		ch.epfl.dias.ops.vector.Scan scan = new ch.epfl.dias.ops.vector.Scan(lineItem,500);
+		ch.epfl.dias.ops.vector.Select sel = new ch.epfl.dias.ops.vector.Select(scan, BinaryOp.EQ, 3, 6);
+		ch.epfl.dias.ops.vector.Project prj = new ch.epfl.dias.ops.vector.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		prj.open();
+		DBColumn[] result = prj.next();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryTwoVec(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.vector.Scan scan = new ch.epfl.dias.ops.vector.Scan(lineItem,500);
+		ch.epfl.dias.ops.vector.Select sel = new ch.epfl.dias.ops.vector.Select(scan, BinaryOp.GT, 3, 6);
+		ch.epfl.dias.ops.vector.Project prj = new ch.epfl.dias.ops.vector.Project(sel, new int[]{0,4});
+
+		long startTime = System.nanoTime();
+		prj.open();
+		DBColumn[] result = prj.next();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryThreeVec(ColumnStore lineItem){
+		// SELECT COUNT (*) FROM LINE_ITEM
+		ch.epfl.dias.ops.vector.Scan scan = new ch.epfl.dias.ops.vector.Scan(lineItem,500);
+		ch.epfl.dias.ops.vector.ProjectAggregate agg = new ch.epfl.dias.ops.vector.ProjectAggregate(scan, Aggregate.COUNT, DataType.INT, 3);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBColumn[] result = agg.next();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFourVec(ColumnStore lineItem){
+		// Select SELECT L_ORDERKEY, L_quantity FROM LINE_ITEM WHERE col4 > 6
+		ch.epfl.dias.ops.vector.Scan scan = new ch.epfl.dias.ops.vector.Scan(lineItem, 500);
+		ch.epfl.dias.ops.vector.Select sel = new ch.epfl.dias.ops.vector.Select(scan, BinaryOp.GE, 3, 6);
+		ch.epfl.dias.ops.vector.ProjectAggregate agg = new ch.epfl.dias.ops.vector.ProjectAggregate(sel, Aggregate.AVG, DataType.DOUBLE, 4);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBColumn[] result = agg.next();
+		System.out.println(result[0]);
+		long endTime = System.nanoTime();
+		return (endTime - startTime);
+	}
+
+	static public long queryFiveVec(ColumnStore lineItem, ColumnStore orders){
+		// SELECT COUNT(*) FROM order JOIN lineitem ON (o_orderkey = orderkey)
+
+		ch.epfl.dias.ops.vector.Scan scanOrder = new ch.epfl.dias.ops.vector.Scan(orders, 500);
+		ch.epfl.dias.ops.vector.Scan scanLineitem = new ch.epfl.dias.ops.vector.Scan(lineItem, 500);
+
+		ch.epfl.dias.ops.vector.Join join = new ch.epfl.dias.ops.vector.Join(scanLineitem, scanOrder, 0, 0);
+		ch.epfl.dias.ops.vector.ProjectAggregate agg = new ch.epfl.dias.ops.vector.ProjectAggregate(join, Aggregate.COUNT, DataType.INT, 0);
+
+		long startTime = System.nanoTime();
+		agg.open();
+		DBColumn[] result = agg.next();
+		System.out.println(result[0]);
 		long endTime = System.nanoTime();
 		return (endTime - startTime);
 	}
